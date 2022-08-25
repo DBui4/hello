63bf84b828565b5f7891d6728fd8c9168a970526
DBui4 <dbui4@huskers.unl.edu>
2022-08-25 13:53:25 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Dennis Bui
+ *         dbui4@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Dennis!!\n");
+  printf("My major is Computer Science!\n");
+  return 0;
+}
