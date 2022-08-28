1a19f338d5f20ed536cb58aa91a4653da36c6991
Seamus McNear <smcnear4@huskers.unl.edu>
2022-08-28 23:55:52 +0000

    added my major too

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@
 int main(int argc, char **argv) {
 
   printf("Hello Dennis!!\n");
-  printf("My major is Computer Science!\n");
+  printf("Hello Seamus!\n");
+  printf("Our majors are Computer Science!\n");
+  printf("Minor is Economics\n");
   return 0;
 }
